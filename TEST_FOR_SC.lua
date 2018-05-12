063033d274aa881f7bce6a4659f4a7671ea0fabc
ascended4 <36668448+ascended4@users.noreply.github.com>
2018-05-12 14:57:51 +0300

    Add files via upload

--- TEST_FOR_SC.lua
+++ TEST_FOR_SC.lua
@@ -1,1 +1,10 @@
-game test_ai
+{
+	ASCENDED_TEST_TABLE.test1 = 10
+	ASCENDED_TEST_TABLE.test2 = "10"
+	ASCENDED_TEST_TABLE.test3 = 3.0
+}
+ASCENDED_TEST_TABLE = {
+	test1 = 10
+	test2 = "10"
+	test3 = 3.0
+}
